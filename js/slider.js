8cc0454883c38cbe4bafb617abef1a80e81f92e5
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-17 00:45:27 +0600

    added js file and setinterval

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+
+const images =[
+    'img/img-1.jpg',
+    'img/img-2.jpg',
+    'img/img-3.jpg',
+    'img/img-4.jpg',
+    'img/img-5.jpg',
+    'img/img-6.jpg',
+    'img/img-7.jpg',
+    'img/img-8.jpg',
+    'img/img-9.jpg',
+    'img/img-10.jpg',
+    'img/img-11.jpg',
+    'img/img-12.jpg',
+    'img/img-13.jpg',
+   
+]
+
+
+let imgIndex = 0;
+setInterval(()=>{
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    const imgUrl =images[imgIndex];
+    console.log(imgUrl);
+
+    imgIndex ++;
+}, 1500);
